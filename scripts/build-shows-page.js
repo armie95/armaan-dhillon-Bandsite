b4799d58e45ce85dc942045d8b30c3a60fcb2857
armie95 <115040104+armie95@users.noreply.github.com>
2022-10-30 18:24:40 -0400

    Added the functionality for the row selection in tickets/shows page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,147 +1,114 @@
 // Create the section element :
-const showsSection = document.querySelector("#shows")
+const showsSection = document.querySelector("#shows");
 
 // create the shows title and adding the wrapper class to it:
-const showsTitle = document.createElement("h2")
-showsTitle.classList.add("shows__title", "wrapper")
+const showsTitle = document.createElement("h2");
+showsTitle.classList.add("shows__title", "wrapper");
 // showsTitle.innerText = "Shows"
-showsTitle.textContent = "Shows"
+showsTitle.textContent = "Shows";
 
-showsSection.append(showsTitle)
+showsSection.append(showsTitle);
 
-const BASE_URL = "https://project-1-api.herokuapp.com/"
+const BASE_URL = "https://project-1-api.herokuapp.com/";
 const API_KEY = "?api_key=9100b3a8-05e5-4ee3-9514-3198df6acb60";
 
 const buildURL = (endPoint) => {
-    return `${BASE_URL}${endPoint}/$ {API_Key}`
-}
+  return `${BASE_URL}${endPoint}/$ {API_Key}`;
+};
 
+const ticketElementsArray = [];
 /*Gets the show dates for the bands and console log into the browser url  getting the response*/
 /* we used the fetch method and created 2 varables holding our base url for the backend endpoint and and api key hlding the credentials*/
- /* we will working on displayng onto page. */
-fetch(`${BASE_URL}showdates/${API_KEY}`, {
-    method: "GET",
-    headers: { "Content-type": "application/json" }
-}).then(res => {
-    return res.json()
-}).then(data => {
-    data.forEach(showdates =>{
-        const markUp = `<ul >
-        <div class="">${showdates.id}</div>
-        <div class="comment-name-style">${showdates.date}</div>
-        <div class="comment-comment-style">${showdates.place}</div>
-        <div>${showdates.location}</div>
-        </ul>`;
-        document.getElementById('shows').insertAdjacentHTML("beforeend", markUp) })
-    console.log(data);
-}).catch(err => {
+/* we will working on displayng onto page. */
+axios
+  .get(`${BASE_URL}showdates/${API_KEY}`)
+  .then((data) => {
+    const showTickets = data.data;
+    showTickets.forEach((ticket) => {
+      displayTicket(ticket);
+    });
+  })
+  .catch((err) => {
     console.log(err.message);
-});
-
-
-// // ! The tickets Array:
-// const ticketsList = [
-//     {
-//         date: new Date(),
-//         venue: "Ronald Lane",
-//         location: "San Frrancisco, CA"
-//     },
-//     {
-//         date: new Date(),
-//         venue: "Pier 3 East",
-//         location: "San Frrancisco, CA"
-//     },
-//     {
-//         date: new Date(),
-//         venue: "Ronald Lane",
-//         location: "San Frrancisco, CA"
-//     },
-//     {
-//         date: new Date(),
-//         venue: "Pier 3 East",
-//         location: "San Frrancisco, CA"
-//     },
-// ]
-
-
-for (let i = 0; i < ticketsList.length; i++) {
-    displayTicket(ticketsList[i])
-}
-
+  });
 
 function displayTicket(ticket) {
+  // Create a show div:
+  const showDiv = document.createElement("div");
+  showDiv.classList.add("shows__show");
 
-    // Create a show div:
-    const showDiv = document.createElement("div")
-    showDiv.classList.add("shows__show")
+  //create the wrapper div:
+  const wrapperDiv = document.createElement("div");
+  wrapperDiv.classList.add("wrapper");
 
-    //create the wrapper div:
-    const wrapperDiv = document.createElement("div")
-    wrapperDiv.classList.add("wrapper")
+  // create a row for the current ticket's date:
+  const dateRow = document.createElement("div");
+  dateRow.classList.add("shows__row");
 
-    // create a row for the current ticket's date:
-    const dateRow = document.createElement("div")
-    dateRow.classList.add("shows__row")
+  // create a row for the current ticket's title:
+  const titleRow = document.createElement("div");
+  titleRow.classList.add("shows__row");
 
-    // create a row for the current ticket's title:
-    const titleRow = document.createElement("div")
-    titleRow.classList.add("shows__row")
+  // create a row for the current ticket's location:
+  const locationRow = document.createElement("div");
+  locationRow.classList.add("shows__row");
 
-    // create a row for the current ticket's location:
-    const locationRow = document.createElement("div")
-    locationRow.classList.add("shows__row")
+  // create label for the date:
+  const dateLabel = document.createElement("label");
+  dateLabel.classList.add("shows__label");
+  dateLabel.textContent = "Date";
 
+  // create h3 for the date text:
+  const dateText = document.createElement("h3");
+  dateText.classList.add("shows__text");
+  const dateToDateString = new Date(ticket.date).toLocaleDateString();
+  dateText.textContent = dateToDateString;
 
-    // create label for the date:
-    const dateLabel = document.createElement("label")
-    dateLabel.classList.add("shows__label")
-    dateLabel.textContent = "Date"
+  // create label for the title (place):
+  const titleLabel = document.createElement("label");
+  titleLabel.classList.add("shows__label");
+  titleLabel.textContent = "Venue";
 
-    // create h3 for the date text:
-    const dateText = document.createElement("h3")
-    dateText.classList.add("shows__text")
-    dateText.textContent = ticket.date.toDateString()
+  // create h3 for the title (place) text:
+  const titleText = document.createElement("h3");
+  titleText.classList.add("shows_text");
+  titleText.textContent = ticket.place;
 
+  // create label for the location:
+  const locationLabel = document.createElement("label");
+  locationLabel.classList.add("shows__label");
+  locationLabel.textContent = "Location";
 
-    // create label for the title (venue):
-    const titleLabel = document.createElement("label")
-    titleLabel.classList.add("shows__label")
-    titleLabel.textContent = "Venue"
+  // create h3 for the location text:
+  const locationText = document.createElement("h3");
+  locationText.classList.add("shows_text");
+  locationText.textContent = ticket.location;
 
-    // create h3 for the title (venue) text:
-    const titleText = document.createElement("h3")
-    titleText.classList.add("shows_text")
-    titleText.textContent = ticket.venue
+  // create the buy ticket btn:
+  const buyBtn = document.createElement("button");
+  buyBtn.classList.add("shows__buy-tickets");
+  buyBtn.innerText = "Buy Tickets";
 
+  //! Append the childern to their parenet containers or divs or sections:
+  dateRow.append(dateLabel, dateText);
 
-    // create label for the location:
-    const locationLabel = document.createElement("label")
-    locationLabel.classList.add("shows__label")
-    locationLabel.textContent = "Location"
+  titleRow.append(titleLabel, titleText);
 
-    // create h3 for the location text:
-    const locationText = document.createElement("h3")
-    locationText.classList.add("shows_text")
-    locationText.textContent = ticket.location
+  locationRow.append(locationLabel, locationText);
 
+  wrapperDiv.append(dateRow, titleRow, locationRow, buyBtn);
 
-    // create the buy ticket btn:
-    const buyBtn = document.createElement("button")
-    buyBtn.classList.add("shows__buy-tickets")
-    buyBtn.innerText = "Buy Tickets"
+  showDiv.append(wrapperDiv);
+  showsSection.append(showDiv);
 
+  //! Select Show ticket functionality:
+  showDiv.addEventListener("click", (e) => {
+    ticketElementsArray.forEach((ele) => {
+      ele.classList.remove("shows__show__selected");
+    });
 
-    //! Append the childern to their parenet containers or divs or sections:
-    dateRow.append(dateLabel, dateText)
+    e.currentTarget.classList.add("shows__show__selected");
+  });
 
-    titleRow.append(titleLabel, titleText)
-
-    locationRow.append(locationLabel, locationText)
-
-    wrapperDiv.append(dateRow, titleRow, locationRow, buyBtn)
-
-    showDiv.append(wrapperDiv)
-
-    showsSection.append(showDiv)
+  ticketElementsArray.push(showDiv);
 }
-
